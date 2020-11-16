6395a74fb7a85ffd0d497739e17fc1fd73178204
Johan <johan27000@gmail.com>
2020-11-16 23:15:25 +0100

    add footer and navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,8 @@
-import NavbarIndex from './components/NavbarIndex.vue';
-import Footer from './components/Footer.vue';
+
 import '../styles/tailwind.css';
 import Vue from 'vue';
 
-	new Vue({
-		el: '#navbarIndex',
-		render: h => h(NavbarIndex)
-	  });
 
 	  new Vue({
-		el: '#footer',
-		render: h => h(Footer)
+		
 	  });
