a7696eaec7f2d9d91ecd90b4a1422a8175ee9760
Johan <johan27000@gmail.com>
2020-11-20 11:50:01 +0100

    fix modal

--- assets/js/components/login/ButtonLogin.vue
+++ assets/js/components/login/ButtonLogin.vue
@@ -1,49 +1,50 @@
 <template>
   <div>
-    <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" v-on:click.prevent="toggleModal()">
+    <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" v-on:click.prevent="showModal = true">
       Se connecter
     </button>
-    <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+
+  <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
       <div class="relative w-auto my-6 mx-auto max-w-6xl">
-        <!--content-->
+        
         <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-          <!--header-->
+         
           <div class="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
             <h3 class="text-3xl font-semibold">
               Connexion
             </h3>
-            <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" v-on:click.prevent="toggleModal()">
+            <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" v-on:click.prevent="showModal = false">
               <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                 ×
               </span>
             </button>
           </div>
-          <!--body-->
+          
           <div class="relative p-6 flex-auto">
             <h1 class="text-center text-2xl text-green-dark">Login</h1>
 
             <form class="pt-6 pb-2 my-2">
               <div class="mb-4">
                 <label class="block text-sm font-bold mb-2" for="email">
-                  Email Address
+                  Email 
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="email" type="text" placeholder="Email Address">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="email" type="text" placeholder="Email ">
               </div>
               <div class="mb-6">
-                <label class="block text-sm font-bold mb-2" for="password">
-                  Password
+                <label class="block text-sm font-bold mb-2" for="Mot de">
+                  Mot de passe
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Mot de passe">
               </div>
               <div class="block md:flex items-center justify-between">
                 <div>
                   <button class="bg-green hover:bg-green-dark text-black font-bold py-2 px-4 rounded border-b-4 border-green-darkest" type="button">
-                    Sign In
+                   Se connecter
                   </button>
                 </div>
 
                 <div class="mt-4 md:mt-0">
-                  <a href="#" class="text-green no-underline">Forget Password?</a>
+                  <a href="#" class="text-green no-underline">Mot de passe oublié?</a>
                 </div>
               </div>
             </form>
@@ -52,7 +53,7 @@
         </div>
       </div>
     </div>
-    <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div> 
   </div>
 </template>
 
@@ -64,10 +65,6 @@ export default {
       showModal: false
     }
   },
-  methods: {
-    toggleModal: function(){
-      this.showModal = !this.showModal;
-    }
-  }
+
 }
 </script>
